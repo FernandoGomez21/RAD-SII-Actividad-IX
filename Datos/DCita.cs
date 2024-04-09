302da9f354464b59240178dcff17770020c065fb
ferna <ferna@FERNANDOGOMEZ>
2024-04-09 16:33:59 -0600

    Capa de Datos Finalizada

---
+++ Datos/DCita.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos.BaseDatos.Models;
+using Datos.Core;
+
+namespace Datos
+{
+    public class DCita
+    {
+        UnitOfWork _unitOfWork;
+        public DCita() {
+          _unitOfWork = new UnitOfWork();
+        }
+        public int CitaId { get; set; }
+
+        public int MedicoId { get; set; }
+        public Medico Medico { get; set; }
+
+        public int PacienteId { get; set; }
+        public Paciente Paciente { get; set; }
+
+        public DateTime FechaCita { get; set; }
+        public bool Estado { get; set; }
+
+
+        public List<Cita> TodosLasCitas()
+        {
+            return _unitOfWork.Repository<Cita>().Consulta().ToList();
+        }
+
+        public int GuardarCita(Cita cita)
+        {
+            if (cita.CitaId == 0)
+            {
+                _unitOfWork.Repository<Cita>().Agregar(cita);
+                return _unitOfWork.Guardar();
+            }
+
+            else
+            {
+                var CitaeInDb = _unitOfWork.Repository<Cita>().Consulta().FirstOrDefault(c => c.CitaId == cita.CitaId);
+
+                if (CitaeInDb == null)
+                {
+                    CitaeInDb.MedicoId = cita.MedicoId;
+                    CitaeInDb.PacienteId = cita.PacienteId;
+                    CitaeInDb.FechaCita = cita.FechaCita;
+                    CitaeInDb.Estado = cita.Estado;
+
+                    _unitOfWork.Repository<Cita>().Editar(cita);
+                    return _unitOfWork.Guardar();
+                }
+                return 0;
+            }
+        }
+
+        public int EliminarCita(int citaId)
+        {
+            var CitaeInDb = _unitOfWork.Repository<Cita>().Consulta().FirstOrDefault(c => c.CitaId == citaId);
+
+            if (CitaeInDb == null)
+            {
+                _unitOfWork.Repository<Cita>().Eliminar(CitaeInDb);
+            }
+            return 0;
+        }
+
+    }
+}
---
+++ Datos/DPaciente.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos.BaseDatos.Models;
+using Datos.Core;
+
+namespace Datos
+{
+    internal class DPaciente
+    {
+
+        UnitOfWork _unitOfWork;
+
+        public DPaciente()
+        {
+            _unitOfWork = new UnitOfWork();
+        }
+
+        public int PacienteId { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public bool Estado { get; set; }
+
+        public List<Paciente> TodosLosPcientes()
+        {
+            return _unitOfWork.Repository<Paciente>().Consulta().ToList();
+        }
+
+        public int GuardarPaciente(Paciente paciente)
+        {
+            if (paciente.PacienteId == 0)
+            {
+                _unitOfWork.Repository<Paciente>().Agregar(paciente);
+                return _unitOfWork.Guardar();
+            }
+
+            else
+            {
+                var PacienteInDb = _unitOfWork.Repository<Paciente>().Consulta().FirstOrDefault(c => c.PacienteId == paciente.PacienteId);
+
+                if (PacienteInDb == null)
+                {
+                    PacienteInDb.Nombres = paciente.Nombres;
+                    PacienteInDb.Apellidos = paciente.Apellidos;
+                    PacienteInDb.Estado = paciente.Estado;
+
+                    _unitOfWork.Repository<Paciente>().Editar(paciente);
+                    return _unitOfWork.Guardar();
+                }
+                return 0;
+            }
+        }
+
+        public int EliminarPaciente(int pacienteId)
+        {
+            var PacienteInDb = _unitOfWork.Repository<Paciente>().Consulta().FirstOrDefault(c => c.PacienteId == pacienteId);
+
+            if (PacienteInDb == null)
+            {
+                _unitOfWork.Repository<Paciente>().Eliminar(PacienteInDb);
+            }
+            return 0;
+        }
+
+    }
+}
