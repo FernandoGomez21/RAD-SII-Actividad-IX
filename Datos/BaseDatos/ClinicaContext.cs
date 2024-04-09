3ebb6385a76990307a7d3d6faffd30550edd73eb
ferna <ferna@FERNANDOGOMEZ>
2024-04-09 15:52:46 -0600

    Agregar archivos de proyecto.

---
+++ Datos/BaseDatos/ClinicaContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.BaseDatos
+{
+    public class ClinicaContext : DbContext
+    {
+        public ClinicaContext() : base("name= ClinicaMedica")
+        {
+        
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>(); //Utilizamos using System.Data.Entity.ModelConfiguration.Conventions;
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>(); //No queremos Nombres en plural
+        }
+
+    }
+}
